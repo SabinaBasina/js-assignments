2f97f0da74530dfdef6b753178c1f9b77c0d482d
SabinaBasina <sabina98bel@mail.ru>
2019-02-20 19:58:11 +0300

    some04

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -324,7 +324,8 @@ function getPositivesCount(arr) {
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
 function sortDigitNamesByNumericOrder(arr) {
-  throw new Error('Not implemented');
+  var m = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6,
+    'seven':7, 'eight':8, 'nine':9}; return arr.sort((a, b)=>m[a]-m[b]);
 }
 
 /**
